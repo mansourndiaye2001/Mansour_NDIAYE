22b99c3cb25b6cab83357f47644f1b646e5066a4
mansourndiaye <ndiayemansour20001@gmail.com>
2024-12-17 14:31:44 +0100

    second commit

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
  // Fonction principale pour afficher le message 
 function main() { 
- echo "Hello, GitHub!"; 
+// echo "Hello, GitHub!"; 
+echo "TP github";
 } 
 // Appel de la fonction principale 
 main(); 
